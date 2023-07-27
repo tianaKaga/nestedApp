cd7b19d8d2049b8abab1d1ee01ff6b582f101a43
Tiana Kaga <tiana.kaga@gmail.com>
2023-07-27 13:09:38 -0400

    Initial Commit

---
+++ nestedApp/nestedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nestedAppApp.swift
+//  nestedApp
+//
+//  Created by Scholar on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct nestedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
